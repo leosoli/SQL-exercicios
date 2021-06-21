60558ff5d69168f60e22b751481fcdabd60a8cda
Leonardo de Souza Oliveira <l.leosoliveira@gmail.com>
2021-06-21 16:11:33 -0300

    query_ex3.sql: correção e ajuste

--- Exercicio3/query_ex3.sql
+++ Exercicio3/query_ex3.sql
@@ -1,11 +1,11 @@
 select
     b.name,
-    sum(amount) as sum
+    sum(a.amount) as sum
 from
     products a
-        inner join
-            categories b
-        on a.id_categories = b.id
+inner join
+    categories b
+on a.id_categories = b.id
 group by
     b.name
 order by
